5b6e4372a0065a4266ddde099e75b4d82b8075a2
André Garcia Fattore <89103821+L0boDoMar@users.noreply.github.com>
2021-10-07 17:47:27 -0300

    Update ex17.js

--- exercicios JavaScript/Faccat/scripts/ex17.js
+++ exercicios JavaScript/Faccat/scripts/ex17.js
@@ -1,7 +1,7 @@
 /*Ler as notas da 1a. e 2a. avaliações de um aluno. Calcular a média aritmética simples e escrever uma mensagem que diga se o aluno foi ou não aprovado (considerar que nota igual ou maior que 6 o aluno é aprovado). Escrever também a média calculada. */
 
-var a1 = parseInt(prompt("Infome a nota 1a:"));
-var a2 = parseInt(prompt("Infome a nota 2a:"));
+var a1 = parseFLoat(prompt("Infome a nota 1a:"));
+var a2 = parseFloat(prompt("Infome a nota 2a:"));
 
 var mediaFinal= (a1+a2)/2;
 
